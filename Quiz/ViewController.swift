0b2089afad51c40efb36feaae42e732fd09541c8
piti <90437140+pitiflutter@users.noreply.github.com>
2023-07-26 18:57:20 +0330

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Hamed on 7/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
